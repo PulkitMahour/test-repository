05b8a041dd163a1ee7dcf1af20e21aeb24b38fa0
Pulkit Mahaur <pulkitmahour15jan@gmail.com>
2021-09-18 17:22:14 +0530

    Removing commented code

--- server.py
+++ server.py
@@ -19,17 +19,6 @@ class handler(BaseHTTPRequestHandler):
 		self.send_header('Content-type','text/html')
 		self.end_headers()
 
-
-# def generate_actual_stuff(n):
-# 	start ="<img src=\"https://d308ljkq6e62o1.cloudfront.net/img/ni0nIiXtRHy77GQF5i5ZlA/mobile/file\">" +\
-# 				"<ol>" 
-# 	for i in range(n):
-# 		start += "<li><marquee><b>"+ str(i) + "</b> <u>"+ str(random.random())+ "</u></marquee></li>"
-# 	start +=				"</ol>"
-# 	return start
-
-
-		# if self.path[1:] == '':
 		basic_output_start = "<html>" + \
 		"<head>"+ \
 			"<title>My Title</title>" +\
@@ -38,8 +27,6 @@ class handler(BaseHTTPRequestHandler):
 		"<ul>" 
 		for i in range(3):
 			basic_output_start += "<li>" + "TEST" + "</li>"
-		# "<li>" + "TEST" + "</li>" +\
-		# "<li>" + "TEST" + "</li>"+\
 
 
 
@@ -47,46 +34,8 @@ class handler(BaseHTTPRequestHandler):
 		"</body>" + \
 		"</html>"
 
-		# b()
-		# "<ol>"
-
-		# for i in lst:
-		# 	a = "<li>"+str(i)+"</li>"
-		# "</ol>"
-			# self.wfile.write(bytes(i, 'utf8'))
-		# "</body>" + \
-		# "</html>"
-
-
-			# actual_stuff = generate_actual_stuff(int(self.path[1:]))
-			# basic_output_end =			"</body>"+ \
-			# "</html>"
-			# output = basic_output_start + actual_stuff + basic_output_end
 		self.wfile.write(bytes(basic_output_start,'utf-8'))
 
-			# for i in lst:
-			# 	self.wfile.write(bytes(i, 'utf8'))
-		
-		# if self.path[1:] != '':
-
-		# 	if os.path.isdir(self.path[1:]):
-		# 		os.chdir(self.path[1:])
-		# 		lst1 = os.listdir()
-		# 		for j in lst1:
-		# 			self.wfile.write(bytes(j, 'utf8'))
-		# 		x.append(self.path)
-
-		# 	elif os.path.isfile(self.path[1:]):
-		# 		with open(self.path[1:], 'rb')as f:
-		# 			self.wfile.write(f.read())
-
-		# 	elif x[0] in self.path:
-		# 		y = self.path.replace(x[0], '').replace('/','')
-		# 		print(x)
-		# 		print(y)
-		# 		if os.path.isfile(y):
-		# 			with open(y, 'rb')as f:
-		# 				self.wfile.write(f.read())
 
 try:
 	with HTTPServer(('localhost', 1233), handler) as server:
@@ -95,19 +44,3 @@ except KeyboardInterrupt:
 	pass
 
 
-# import os
-# from http.server import HTTPServer, CGIHTTPRequestHandler
-
-# os.chdir('.')
-# server_object = HTTPServer(server_address=('', 1234), RequestHandlerClass=CGIHTTPRequestHandler)
-# server_object.serve_forever()
-
-
-# jk = 'txt_files/data.txt'
-# l = ['txt_files']
-
-
-# if l[0] in jk:
-# 	print('yes')
-# 	i = jk.replace('txt_files','')
-# 	print(i)
